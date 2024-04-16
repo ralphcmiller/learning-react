91ed6f4d71460656e8d71c381e834ad5dc660bc3
Ralph <ralphcmille@gmail.com>
2024-04-16 11:33:25 -0600

    implemented state management

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 function ListGroup() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Group</h1>
+      {items.length === 0 && <p>No items to display</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
