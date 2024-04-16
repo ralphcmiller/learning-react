bdd32dcaf79b712f933793c93e7542fd415544c3
Ralph <ralphcmille@gmail.com>
2024-04-16 11:39:23 -0600

    implemented props for component reusability

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
