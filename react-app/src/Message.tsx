0e336aaf87be67f7bb3a487a6d43a134ad6cdf41
Ralph <ralphcmille@gmail.com>
2024-04-16 10:27:47 -0600

    Built my first component!

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing is used for the name of the file and the name of the component
+function Message() {
+    // JSX: JavaScript XML
+    const name = "Ralph";
+    if (name)
+        return <h1>Hey, {name}</h1>;
+    return <h1>Hello World!</h1>;
+}
+
+export default Message;
